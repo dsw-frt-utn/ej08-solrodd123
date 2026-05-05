a3ec0f785b9424eaadde067ca1e7fb9e0828eea9
Maria Sol Rodriguez <solrodd123@gmail.com>
2026-05-04 22:20:57 -0300

    Resolucion Problema 3- Tipos por valor y referencia.

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copiaValue = originalValue;
+
+            copiaValue++;
+
+            Product copiaProduct = product;
+
+            copiaProduct.DescriptionProduct = "Descripcion Modificada";
+
+            return $"{originalValue}-{copiaValue}-{product.DescriptionProduct}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        public string DescriptionProduct { get; set; }
+    }
+}
