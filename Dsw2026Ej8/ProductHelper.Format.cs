2a8dc7f15c60696d19208329597e28e4b809b25b
Maria Sol Rodriguez <solrodd123@gmail.com>
2026-05-04 22:12:30 -0300

    Resolucion Problema 1- Clase Parcial

---
+++ Dsw2026Ej8/ProductHelper.Format.cs
@@ -0,0 +1,7 @@
+﻿namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        internal const string FormatoEtiqueta = "[{0}] {1} - {2:C}";
+    }
+}
---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            return string.Format(FormatoEtiqueta, code, description, price);
+        }
+    }
+}
