f7496a0037ee62bf104917d3d541049a82b2ab0d
Maria Sol Rodriguez <solrodd123@gmail.com>
2026-05-04 22:31:50 -0300

    Resolucion Problema 6- Metodos de Extension.

---
+++ Dsw2026Ej8/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Dsw2026Ej8
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string value)
+        {
+            // Si el código es null, debe retornar "SIN-CODIGO"
+            if (value == null) return "SIN-CODIGO";
+
+            // 1. Eliminar espacios al inicio y final (Trim)
+            // 2. Reemplazar espacios internos por guiones (Replace)
+            // 3. Convertir a mayúsculas (ToUpper)
+            return value.Trim().Replace(" ", "-").ToUpper();
+        }
+    }
+}
---
+++ Dsw2026Ej8/Problema6.cs
@@ -0,0 +1,11 @@
+﻿namespace Dsw2026Ej8
+{
+    public class Problema6
+    {
+        public string NormalizarCodigoProducto(string code)
+        {
+            // Llama al método de extensión que creamos arriba
+            return code.ToProductCode();
+        }
+    }
+}
